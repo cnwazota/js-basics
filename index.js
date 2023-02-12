58e5e53da004f74ef7adac6fffab655611e71d27
cnwazota <chinenyenwazota@gmail.com>
2023-02-12 18:30:27 -0500

    JavaScript Basics

---
+++ index.js
@@ -0,0 +1,14 @@
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(3, 5))
+
+
+let firstName = "Chinenye";
+let lastName = "Nwazota";
+let track = "Frontend Development";
+let skills = "Analytical, Critical-thinking, Problem solving";
+let favouriteColor = "Russet";
+let countryIndependence = "1960";
+
+console.log("My name is " + firstName, lastName + ". I am a student at Stutern in the " + track + " track" + ". My skills are: " + skills + ". I love the color: " + favouriteColor + ". I am from Nigeria. Nigeria gained its' Independence in the year " + countryIndependence + ".");
